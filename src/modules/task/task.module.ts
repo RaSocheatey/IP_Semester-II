2ed72c9068156d772c749f0f0b6c3211521644c9
CHEATEY13 <dixlina2@gmail.com>
2026-02-20 02:09:58 +0700

    Step 1 & 2 complete: TypeORM and SQLite setup

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './task.controller';
 import { TaskService } from './task.service';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { Task } from './task.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Task]), UsersModule],
   controllers: [TasksController],
   providers: [TaskService],
-  exports: [],
+  exports: [TypeOrmModule],
   // Add any other necessary configurations or modules
 })
 export class TaskModule {}
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './user.controller';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UserService],
-  exports: [],
+  exports: [TypeOrmModule],
   // Add any other necessary configurations or modules
 })
 export class UserModule {}
